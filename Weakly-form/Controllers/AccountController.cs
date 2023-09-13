e6f977bf404933295eb6b6d51603a4a3aab11711
Masoud <Masoud.fathi@gmail.com>
2023-09-13 12:34:40 +0330

    Add project files.

---
+++ Weakly-form/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Weakly_form.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult LoginPost(string Username, string Password)
+        {
+            // check if username and password are matched or not
+            bool matched = true;
+            if (matched)
+            {
+                ViewBag.Username = Username;
+                return View();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
